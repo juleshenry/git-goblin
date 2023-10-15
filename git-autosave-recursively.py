4ae0d7508c5b4520ed6b83c17772aa25c50ef80b
Julian Henry <julianplushenry@gmail.com>
2023-10-15 18:13:32 -0500

    Autosaving... Sun Oct 15 18:13:32 CDT 2023

--- git-autosave-recursively.py
+++ git-autosave-recursively.py
@@ -7,25 +7,21 @@ def unseen(gfc, dir):
             return 0
     return 1
 
-def change_directory_and_run_command(directory, command):
-    gacp = "git add .; git commit -m 'Autosaving...';git push"
+def change_directory_and_run_command(directory):
+    gacp = "git add .; git commit -m 'Autosaving... $(date)';git push"
     rez = subprocess.run([f'cd {directory}; zsh -i -c "{gacp }"'], shell=True)
     print(rez.stdout)
 
 def recurse_directories(base_path):
     gitful_cache = set() # dirs with git in them
-    autosave = "gacp '"
     for root, dirs, files in os.walk(base_path):
-        
         for d in dirs:
             command = os.path.join(root, d)
             # checks if in a Git repo
             if unseen(gitful_cache, command) and '.git' in str(command):
                 gitful_cache.add(command)
-                # print('Git repo discovered     @', command[:-5])
                 place=(command[2:-4])
-                print(place)
-                change_directory_and_run_command(place, autosave)
+                change_directory_and_run_command(place)
 
 if __name__ == "__main__":
     base_path = "."  # Starting directory (current directory)
