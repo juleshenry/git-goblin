22c67245b873e86f3e9bcedaed51c3a893ffaba0
Juelz <juliantx@naver.com>
2025-10-06 08:49:12 -0500

    insipido-aburrido

--- git-goblin.py
+++ git-goblin.py
@@ -3,6 +3,6 @@ alias_files = []
 with open("README.md") as s:
     for o in sorted(s.readlines()):
         if "#" in o and "py" not in o and "##" not in o and "git-goblin" not in o:
-            print(f"alias {(g:=o.split('#')[1][1:-1])}={os.getcwd()}/git-goblin/{g}")
+            print(f"alias {(g:=o.split('#')[1][1:-1])}={os.getcwd()}/{g}")
             alias_files+=[g]
 print('chmod +x '+ ' '.join(list(alias_files)))
