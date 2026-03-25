60a525b2e63071150bb2f3a1ed6467c94055dc7b
Juelz <juliantx@naver.com>
2026-03-24 22:42:30 -0500

    feat: add macos power tools and super utilities

---
+++ modules/macos_power_tools.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# macOS Power Tools
+
+_GG_REGISTRY["caffeinate"]="caffeinate -d ||| Prevent Mac from sleeping"
+_GG_REGISTRY["hidefiles"]="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder ||| Hide hidden files in Finder"
+_GG_REGISTRY["showfiles"]="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder ||| Show hidden files in Finder"
+_GG_REGISTRY["flushui"]="killall Dock; killall Finder ||| Restart Dock and Finder to fix UI glitches"
+_GG_REGISTRY["copyurl"]="osascript -e 'tell app \"Safari\" to get URL of front document' | pbcopy ||| Copy URL of active Safari tab"
+_GG_REGISTRY["copycwd"]="pwd | tr -d '\n' | pbcopy ||| Copy current directory path to clipboard"
+_GG_REGISTRY["locks"]="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend ||| Lock mac screen immediately"
+_GG_REGISTRY["emptytrash"]="rm -rf ~/.Trash/* ||| Empty the Trash immediately"
+
+alias caffeinate='caffeinate -d'
+alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
+alias showfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
+alias flushui='killall Dock; killall Finder'
+alias copyurl='osascript -e "tell app \"Safari\" to get URL of front document" | pbcopy; echo "Safari URL copied to clipboard!"'
+alias copycwd='pwd | tr -d "\n" | pbcopy; echo "Copied $(pwd) to clipboard!"'
+alias locks='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend 2>/dev/null || pmset displaysleepnow'
+alias emptytrash='rm -rf ~/.Trash/*; echo "Trash emptied."'
---
+++ modules/super_utilities.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Super Utilities
+
+_GG_REGISTRY["cheat"]="curl cheat.sh/CMD ||| Get cheat sheet for any CLI command"
+_GG_REGISTRY["weather"]="curl wttr.in/LOCATION ||| Get the weather forecast in terminal"
+_GG_REGISTRY["qrgen"]="curl qrencode.com/STRING ||| Generate a QR code in the terminal"
+_GG_REGISTRY["uuid"]="uuidgen | tr '[:upper:]' '[:lower:]' ||| Generate a random UUIDv4"
+_GG_REGISTRY["epoch"]="date +%s ||| Get current Unix Epoch timestamp"
+_GG_REGISTRY["stopwatch"]="time read ||| Start a simple stopwatch (press Enter to stop)"
+_GG_REGISTRY["timer"]="sleep N && echo 'Time is up!' ||| Simple countdown timer"
+_GG_REGISTRY["matrix"]="Pure Bash Matrix Effect ||| Run the Matrix digital rain in your terminal"
+
+cheat() {
+    if [ -z "$1" ]; then echo "Usage: cheat <command> (e.g. cheat tar)"; return 1; fi
+    curl "cheat.sh/$1"
+}
+
+weather() {
+    curl "wttr.in/${1}"
+}
+
+qrgen() {
+    if [ -z "$1" ]; then echo "Usage: qrgen <string_or_url>"; return 1; fi
+    curl "qrencode.com/$1"
+}
+
+alias uuid='uuidgen | tr "[:upper:]" "[:lower:]"'
+alias epoch='date +%s'
+alias stopwatch='echo "Press ENTER to stop..."; time read'
+
+timer() {
+    if [ -z "$1" ]; then echo "Usage: timer <seconds>"; return 1; fi
+    echo "Timer set for $1 seconds..."
+    sleep "$1"
+    echo -e "\007Time is up!"
+}
+
+alias matrix='LC_ALL=C tr -c "[:digit:]" " " < /dev/urandom | dd cbs=$COLUMNS conv=unblock 2>/dev/null | GREP_COLOR="1;32" grep --color "[^ ]"'
